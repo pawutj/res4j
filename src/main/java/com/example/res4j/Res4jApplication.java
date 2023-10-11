71708c933d9848d0d03bbe8e39e7ff83417b72e6
pawutjingjit <pawut.jingjit@scbtechx.io>
2023-10-11 15:51:00 +0700

    init

---
+++ src/main/java/com/example/res4j/Res4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.res4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Res4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Res4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/res4j/Res4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.res4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Res4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
